e2583d2c44ba403256160b8b8ca15a58b337571c
William del Rosario <williamdelrosario@yahoo.com>
2019-03-27 09:34:12 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 2019-03-27
+ * Time: 09:26
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+
+// use App\Format;
+// use App\Format as F;
+// use App\Format\{JSON,XML,YAML}
+// use App\Format\JSON;
+// use App\Format\XML;
+// use App\Format\YAML;
+
+// $json = new App\Format\JSON();
+// $xml = new App\Format\XML();
+// $yml = new App\Format\YAML();
+
+// $json = new F\JSON();
+// $xml = new F\XML();
+// $yml = new F\YAML();
+
+// $json = new JSON();
+// $xml = new XML();
+// $yml = new YAML();
+
+print_r("Namespaces");
+// print_r($json);
+// print_r($xml);
+// print_r($yml);
---
+++ src/Format/YAML.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 2019-03-27
+ * Time: 09:29
+ */
+
+namespace App\Format;
+
+class YAML
+{
+
+}
